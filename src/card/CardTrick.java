72b02c3cdc3bea8186b9c07d44cbab07c7d6c53e
sumitkaplish <sumitkaplish13@gmail.com>
2024-05-22 22:57:25 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package card;
 
 import java.util.Random;
@@ -15,53 +11,52 @@ import java.util.Random;
  */
 public class CardTrick {
 
-    public static void main(String[] args) {
-        Card[] magicHand = new Card[7];
-        Random rand = new Random();
-        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+        Random rand = new Random();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
-        // Define the lucky card
-        Card luckyCard = new Card();
-        luckyCard.setValue(4);
-        luckyCard.setSuit("hearts");
+        // Define the lucky card
+        Card luckyCard = new Card();
+        luckyCard.setValue(4);
+        luckyCard.setSuit("Hearts"); 
 
-        // Randomly decide to include the lucky card or not
-        boolean includeLuckyCard = rand.nextBoolean(); // True or false decision
-        int luckyIndex = includeLuckyCard ? rand.nextInt(magicHand.length) : -1; // Set to -1 if not included
+        // Randomly decide to include the lucky card or not
+        boolean includeLuckyCard = rand.nextBoolean();
+        int luckyIndex = includeLuckyCard ? rand.nextInt(magicHand.length) : -1;
 
-        // Fill the magic hand with random cards
-        for (int i = 0; i < magicHand.length; i++) {
-            if (i == luckyIndex) {
-                // Place the lucky card at the determined position
-                magicHand[i] = luckyCard;
-            } else {
-                Card c = new Card();
-                c.setValue(rand.nextInt(13) + 1); // Values from 1 to 13
-                c.setSuit(suits[rand.nextInt(suits.length)]); // Random suit
-                magicHand[i] = c;
-            }
-        }
+        // Fill the magic hand with random cards
+        for (int i = 0; i < magicHand.length; i++) {
+            if (i == luckyIndex) {
+                magicHand[i] = luckyCard;
+            } else {
+                Card c = new Card();
+                c.setValue(rand.nextInt(13) + 1); 
+                c.setSuit(suits[rand.nextInt(suits.length)]);
+                magicHand[i] = c;
+            }
+        }
 
-        // Display all cards in the magic hand
-        System.out.println("Magic Hand Cards:");
-        for (Card card : magicHand) {
-            System.out.println(card.getValue() + " of " + card.getSuit());
-        }
+        // Display all cards in the magic hand
+        System.out.println("Magic Hand Cards:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
 
-        // Check if the lucky card is in the magic hand
-        boolean found = false;
-        for (Card card : magicHand) {
-            if (card.equals(luckyCard)) {
-                found = true;
-                break;
-            }
-        }
+        // Check if the lucky card is in the magic hand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(luckyCard)) {
+                found = true;
+                break;
+            }
+        }
 
-        // Report result for the lucky card
-        if (found) {
-            System.out.println("The lucky card (4 of hearts) is in the magic hand. You win!");
-        } else {
-            System.out.println("The lucky card (4 of hearts) is not in the magic hand. You lose.");
-        }
-    }
+        // Report result for the lucky card
+        if (found) {
+            System.out.println("The lucky card (4 of Hearts) is in the magic hand. You win!");
+        } else {
+            System.out.println("The lucky card (4 of Hearts) is not in the magic hand. You lose.");
+        }
+    }
 }
