ec781815191df506941a5ce3b4d0839df08e6abc
sumitkaplish <sumitkaplish13@gmail.com>
2024-05-22 22:54:49 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -15,54 +15,53 @@ import java.util.Random;
  */
 public class CardTrick {
 
-    public static void main(String[] args) {
-        Card[] magicHand = new Card[7];
-        Random rand = new Random();
-        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    public static void main(String[] args) {
+        Card[] magicHand = new Card[7];
+        Random rand = new Random();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
-        
-        Card luckyCard = new Card();
-        luckyCard.setValue(4);
-        luckyCard.setSuit("SPADES");
+        // Define the lucky card
+        Card luckyCard = new Card();
+        luckyCard.setValue(4);
+        luckyCard.setSuit("hearts");
 
+        // Randomly decide to include the lucky card or not
+        boolean includeLuckyCard = rand.nextBoolean(); // True or false decision
+        int luckyIndex = includeLuckyCard ? rand.nextInt(magicHand.length) : -1; // Set to -1 if not included
 
-        boolean includeLuckyCard = rand.nextBoolean();
-        int luckyIndex = includeLuckyCard ? rand.nextInt(magicHand.length) : -1; 
-        
-        // Fill the magic hand with random cards
-        for (int i = 0; i < magicHand.length; i++) {
-            if (i == luckyIndex) {
-                // Place the lucky card at the determined position
-                magicHand[i] = luckyCard;
-            } else {
-                Card c = new Card();
-                c.setValue(rand.nextInt(13) + 1); // Values from 1 to 13
-                c.setSuit(suits[rand.nextInt(suits.length)]); // Random suit
-                magicHand[i] = c;
-            }
-        }
+        // Fill the magic hand with random cards
+        for (int i = 0; i < magicHand.length; i++) {
+            if (i == luckyIndex) {
+                // Place the lucky card at the determined position
+                magicHand[i] = luckyCard;
+            } else {
+                Card c = new Card();
+                c.setValue(rand.nextInt(13) + 1); // Values from 1 to 13
+                c.setSuit(suits[rand.nextInt(suits.length)]); // Random suit
+                magicHand[i] = c;
+            }
+        }
 
-        // Display all cards in the magic hand
-        System.out.println("Magic Hand Cards:");
-        for (Card card : magicHand) {
-            System.out.println(card.getValue() + " of " + card.getSuit());
-        }
+        // Display all cards in the magic hand
+        System.out.println("Magic Hand Cards:");
+        for (Card card : magicHand) {
+            System.out.println(card.getValue() + " of " + card.getSuit());
+        }
 
-        // Check if the lucky card is in the magic hand
-        boolean found = false;
-        for (Card card : magicHand) {
-            if (card.equals(luckyCard)) {
-                found = true;
-                break;
-            }
-        }
+        // Check if the lucky card is in the magic hand
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(luckyCard)) {
+                found = true;
+                break;
+            }
+        }
 
-        // Report result for the lucky card
-        if (found) {
-            System.out.println("The lucky card (4 of SPADES) is in the magic hand. You win!");
-        } else {
-            System.out.println("The lucky card (4 of SPADES) is not in the magic hand. You lose.");
-        }
-    }
+        // Report result for the lucky card
+        if (found) {
+            System.out.println("The lucky card (4 of hearts) is in the magic hand. You win!");
+        } else {
+            System.out.println("The lucky card (4 of hearts) is not in the magic hand. You lose.");
+        }
+    }
 }
-
